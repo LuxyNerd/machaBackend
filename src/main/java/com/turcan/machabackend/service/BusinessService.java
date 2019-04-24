a8b998ed0578667e9b38eb2958d2f2b99abf950a
ABrumm <artur.brumm@freenet.de>
2019-04-24 12:08:18 +0200

    Create BusinessService.java

---
+++ src/main/java/com/turcan/machabackend/service/BusinessService.java
@@ -0,0 +1,6 @@
+package com.turcan.machabackend.service.BusinessService;
+
+@Service
+public class BusinessService {
+//TODO: Die Logic in den endpoints auf dieser Ebene implementieren und die Endpoints machen nichts anderes als die BusinessService aufzurufen
+}
